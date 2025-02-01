5fd3d55562629f48db3ce68d08e1730fd03a748a
Brandon Winston <brandon@winstn.com>
2025-02-01 09:15:00 -0500

    Define basic CHIP-8 VM structure and all constants

--- src/chip8.h
+++ src/chip8.h
@@ -6,9 +6,20 @@
 
 // CHIP-8 System constants
 #define MEMORY_SIZE 4096
-#define ROM_START_ADDRESS 0x200
+#define NUM_REGISTERS 16
+#define STACK_DEPTH 16
+#define VIDEO_WIDTH 64
+#define VIDEO_HEIGHT 32
+#define NUM_KEYS 16
 #define FONTSET_SIZE 80
+#define ROM_START_ADDRESS 0x200
 
-// TODO: Add more constants and structures
+// Basic CHIP-8 VM structure
+typedef struct {
+    uint8_t memory[MEMORY_SIZE];
+    uint8_t V[NUM_REGISTERS];  // V0-VF registers
+    uint16_t I;                // Index register
+    uint16_t pc;               // Program counter
+} ChipIn_t;
 
 #endif //CHIP8_H
